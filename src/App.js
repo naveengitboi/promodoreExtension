46ff41f5801f640de18151b0b1585af9057f723a
naveengitboi <naveenjangiti9@gmail.com>
2023-02-25 15:19:00 +0530

    styles are done

--- src/App.js
+++ src/App.js
@@ -4,20 +4,28 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="container">
+        <div className='heading'>Concentrate Kar Bro</div>
+        <div className="timer">
+          <div className="hr">
+            <label htmlFor="hour">Hr</label>
+            <input type="number" min="0" max="12" placeholder='00' />
+          </div>
+          <div className="min">
+            <label htmlFor="minutes">Min</label>
+            <input type="number" min={"0"} max="60" placeholder='00' />
+          </div>
+          <div className="sec">
+            <label htmlFor="seconds">Sec</label>
+            <input type="number" min={"0"} max="60" placeholder='00' />
+          </div>
+        </div>
+        <div className="buttonsDiv">
+          <button className='reset'>Reset</button>
+          <button className='stop'>Stop</button>
+          <button className='start'>Start</button>
+        </div>
+      </div>
     </div>
   );
 }
