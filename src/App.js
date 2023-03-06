75b20732df927e21c94e2b4031601fc375bad0c4
naveengitboi <naveenjangiti9@gmail.com>
2023-03-06 23:28:05 +0530

    functionality not added

--- src/App.js
+++ src/App.js
@@ -1,18 +1,33 @@
+import { findAllByDisplayValue } from "@testing-library/react";
 import React, {useState, useEffect} from "react";
 import './App.css'
 function App(){
+
+  const [timeInSec, setTimeInSec] = useState(0)
+  const [hr, setHr] =  useState(0)
+  const [min, setMin] =  useState(0)
+
+  let [runTimer, setRunTimer] = useState(findAllByDisplayValue)
+  useState(() => {
+    if(runTimer){
+      let intervalId = setInterval(() => {
+        
+      }, 1000);
+    }
+  }, [runTimer])
   return(
     <div className="container">
       <div className="timeContainer">
       <h1 className="mainHeading">Concentrate Kar lo Bro</h1>
         <div className="timeDivBox">
-          <input type="number" className="hours" placeholder="Hr" max={"24"}/>
-        <input type="number" className="minutes" placeholder="Min" max={"60"}/>
-        <h1 className="timer">00:00</h1>
+          <input type="number" className="hours" placeholder="Hr" max={"24"} onChange={(e)=> setHr(e.target.value)} />
+        <input type="number" className="minutes" placeholder="Min" max={"60"} onChange={(e)=> setMin(e.target.value)}  />
+        <h1 className="timer">{hr<10 ?`0${hr}` : hr} :
+        {min < 10? `0${min}` : min}</h1>
         <div className="buttonDiv">
           <button className="resetBtn">Reset</button>
           <button className="stopBtn">STOP</button>
-          <button className="startBtn">START</button>
+          <button className="startBtn" onClick={{}}>START</button>
         </div>
         </div>
       </div>
